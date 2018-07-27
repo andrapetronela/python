b867320b9c85b7448b20b5209c010d383f6c116c
Andra <andra.popa1989@gmail.com>
2018-07-27 22:28:22 +0100

    Angry boss

---
+++ angryBoss.py
@@ -0,0 +1,7 @@
+'''Angry boss. Write an angry boss program that rudely asks what you want. 
+Whatever you answer, the angry boss should yell it back to you and then fire you.'''
+
+
+print("What do you need?")
+employer = input()
+print("What do you mean you need " + employer + "?" + " You're fired!")
