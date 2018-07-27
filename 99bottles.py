ae923945b94510bb4ebd51a3a00de1fd68d16e4a
Andra <andra.popa1989@gmail.com>
2018-07-27 22:21:23 +0100

    day4

---
+++ 99bottles.py
@@ -0,0 +1,8 @@
+i = 99
+while i > 0:
+	print(str(i) + " bottles of beer on the wall, " + str(i) + " bottles of beer. \n"
+		+ "Take one down and pass it around, " + str(i) + " bottles of beer on the wall.")
+	print("\n\n")
+	i-=1
+print("0 bottles of beer on the wall, 0 bottles of beer. \n"
+		+ "Take one down and pass it around, 0 bottles of beer on the wall.")
